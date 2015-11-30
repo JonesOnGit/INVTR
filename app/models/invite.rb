2dcdce883084c003baf36da783d70c1710e8e863
Marie Bluett <marie.bluett@gmail.com>
2015-11-30 18:14:38 -0500

    invite model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,10 +3,13 @@ class Invite
 	include Mongoid::Timestamps
 
 	field :name, type: String
+	field :start_date, type: Date
+	field :end_date, type: Date
 	field :description, type: String
+	field :allow_others, type: Boolean 
 
 	validates :name, length: { maximum: 150 }
 	validates :description, length: { maximum: 1000 }
 
-	validates_presence_of :name, :description
+	validates_presence_of :name, :start_date, :end_date, :description, :allow_others
 end
--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Invite, type: :model do
 	it { should validate_presence_of(:description) }
 	it { should validate_length_of(:name).is_at_most(150) }
 	it { should validate_length_of(:description).is_at_most(1000) }
+	it { should validate_presence_of(:start_date) }	 
+	it { should validate_presence_of(:end_date)	}
+	it { should validate_presence_of(:allow_others)	}
+
 end
