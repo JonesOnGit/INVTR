8e4a9f5fbda829a0310805c41b40020306a29707
andygimma <andy.n.gimma@gmail.com>
2015-12-07 11:41:22 -0500

    Remove users controllers and helpers (an associated tests)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :invites
+  devise_for :users
+
 end
 
 # ================================= Routes, Raked ======================================== #
