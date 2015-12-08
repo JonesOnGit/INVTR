6e1e1ee9bd410cd35f67bf3b2ef964c4f3da4d9e
Marie Bluett <marie.bluett@gmail.com>
2015-12-07 22:00:23 -0500

    user controller and testing complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,47 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end 
 
+	def edit 
+			@user = User.find(params[:id])
+	end 
+
+	def create
+		@user = User.new(user_params) 
+		if @user.save
+		    flash[:notice] = "User #{@user.fullname} saved."
+		 	redirect_to user_path(@user)
+		else
+		    flash[:alert] = "User #{@user.fullname} not saved."
+		    render :new
+		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if  @user.update_attributes(user_params)
+		    flash[:notice] = "User #{@user.fullname} successfully updated."
+		    redirect_to user_path(@user)
+		else
+		    flash[:alert] = "User #{@user.fullname} not updated."
+		    redirect_to new_user_path(@user)
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+        @user.destroy
+        flash[:alert] = "User Deleted" 
+
+        #TODO set a better redirect
+        redirect_to new_user_path
+        # redirect_to root_path
+	end
+
+private 
+	
+	def user_params
+		params.require(:user).permit(:email, :password, :encrypted_password, :fullname, :role, :city, :state_or_region, :country, :latitude, :longitude)
+	end 
+
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,11 +17,67 @@ describe UsersController do
       get :show, id: user
     	expect(assigns(:user)).to eq(user)
     end
-   end
 
+    it "renders the :show view" do
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
+      expect(response).to render_template :show
+    end
+  end
+
+  describe "GET #edit" do
+    it "renders an existing User to @user" do 
+      user = FactoryGirl.create(:user)
+      get :edit, id: user
+      expect(assigns(:user)).to eq(user)
+    end 
+
+    it "renders the :edit view" do 
+      user = FactoryGirl.create(:user)
+      get :edit, id: user.id
+      expect(response).to render_template :edit
+    end 
+  end
+
+ 	describe "POST #create" do 
+    it "increments the User database count by 1" do
+      expect {
+        post :create, user: FactoryGirl.attributes_for(:user)
+      }.to change(User, :count).by(1)
+    end
+  end 
+
+  describe "PUT #update" do 
+    it "locates the correct user" do
+      user = FactoryGirl.create(:user)
+      put :update, id: user.id, user: FactoryGirl.attributes_for(:user)
+      expect(assigns(:user)).to eq(user)  
+    end
+
+    it "changes the User's attributes" do
+      user = FactoryGirl.create(:user)
+      put :update, id: user.id, user: FactoryGirl.attributes_for(:user, fullname: "Jessica Jones")
+      user.reload
+      expect(user.fullname).to eq("Jessica Jones")
+    end 
+  end 
+	
+	describe "DELETE #destroy" do
+    it "deletes the user" do
+      user = FactoryGirl.create(:user)
+      expect{
+            delete :destroy, id: user        
+          }.to change(User,:count).by(-1)
+    end
+
+    # it "redirects to the home page" do
+    # end
+  end
 
 
 end 
+
+
 
 
 
