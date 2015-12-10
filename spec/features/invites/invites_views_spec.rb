d1376a05cd0851244343b339640a20082ccd8945
Marie Bluett <marie.bluett@gmail.com>
2015-12-10 18:59:48 -0500

    invite views and testing

--- spec/features/invites/invites_views_spec.rb
+++ spec/features/invites/invites_views_spec.rb
@@ -11,6 +11,32 @@ feature "Invites views" do
         visit invite_path(@invite)
         expect(page).to have_content "#{@invite.name}"
     end
+
+    scenario "Visitor creates a new Invite" do
+   			visit root_path
+
+		    within("#new_invite") do
+            fill_in "Name", :with => "Invite Name"
+            fill_in "Description", :with => "Description text"
+            fill_in "Start date", :with => "12-11-15"
+            fill_in "End date", :with => "12-12-15"
+            click_button "Create Invite"
+        end
+        expect(page).to have_content "Invite Name"
+    end
+
+    scenario "User edits an existing Invite" do
+    	  invite = FactoryGirl.create(:invite)
+   			visit edit_invite_path(invite)
+		    within(".edit_invite") do
+            fill_in "Name", :with => "Invite Name"
+            fill_in "Description", :with => "Description text"
+            fill_in "Start date", :with => "12-11-15"
+            fill_in "End date", :with => "12-12-15"
+            click_button "Update Invite"
+        end
+        expect(page).to have_content "Invite Name"
+    end
 end
 # =================
 # feature "Invites views" do
