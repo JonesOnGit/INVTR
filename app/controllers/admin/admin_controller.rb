d9a88974840bab9ced30e16d0955b09c0d94904a
andygimma <andy.n.gimma@gmail.com>
2015-12-17 00:15:29 -0500

    report table

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,6 +3,12 @@ module Admin
 		before_action :authenticate_user!
 		def index
 			@user = current_user
+			@invite_count = Invite.count
+			@accepted_count = Log.where(type: "accept").count
+			@rejected_count = Log.where(type: "decline").count
+			@report_count = Log.where(type: "report").count
+			@written_report_count = Log.where(type: "create_report").count
+			# @sorted_reports = Invite.where(:report_count.gt => 0).order_by(report_count: "desc")
 		end
 	end
 end
