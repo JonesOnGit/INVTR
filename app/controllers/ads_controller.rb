c8ba710b71aa52b57a8aac7a8295a14327852167
andygimma <andy.n.gimma@gmail.com>
2016-01-21 13:54:53 -0500

    Set up session image

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,7 +1,23 @@
 class AdsController < ApplicationController
 	def session_image
-		f = open('http://s3.amazonaws.com/invtr/ads/avatars/56a1/03e2/dfee/1b00/0300/0000/original/IMAGE_SP.jpg?1453392866')
-		send_file f, :type => 'image/jpeg', :disposition => 'inline'
-		return
+		# check if session image exists
+		# if not, load random image and save as session image
+		# set sesstion to expire in one hour
+		# log which add was used
+		if session[:image].nil?
+			ad = Ad.offset(rand(Ad.count)).first
+			session[:image] = ad.avatar.url
+			begin
+				f = open(session[:image])
+			rescue
+				# f.open("'http://s3.amazonaws.com/invtr/ads/avatars/56a1/03e2/dfee/1b00/0300/0000/original/IMAGE_SP.jpg?1453392866'")
+			end
+			send_file f, :type => 'image/jpeg', :disposition => 'inline'
+			return
+		else
+			f = open(session[:image])
+			send_file f, :type => 'image/jpeg', :disposition => 'inline'
+			return
+		end
 	end
 end
