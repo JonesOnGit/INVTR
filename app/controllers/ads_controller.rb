d452cd48c0bce3e4a99cf215e9c535b590029b19
andygimma <andy.n.gimma@gmail.com>
2016-01-21 19:03:37 -0500

    Proper ad image rescue

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -42,9 +42,10 @@ class AdsController < ApplicationController
 			ad = Ad.offset(rand(Ad.count)).first
 			session[:image] = ad.avatar.url
 			begin
+				binding.pry
 				f = open(session[:image])
 			rescue
-				f.open("'http://s3.amazonaws.com/invtr/ads/avatars/56a1/03e2/dfee/1b00/0300/0000/original/IMAGE_SP.jpg?1453392866'")
+				f = open("http://s3.amazonaws.com/invtr/ads/avatars/56a1/03e2/dfee/1b00/0300/0000/original/IMAGE_SP.jpg?1453392866")
 			end
 			send_file f, :type => 'image/jpeg', :disposition => 'inline'
 			return
