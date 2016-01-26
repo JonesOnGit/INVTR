f9b2b862b642c1345547bbaa7ad20630cc4dd86e
andygimma <andy.n.gimma@gmail.com>
2016-01-26 18:08:37 -0500

    set session[:image]

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -66,11 +66,11 @@ class AdsController < ApplicationController
 		else
 			begin
 				if type == "mobile"
-					i = session[:mobile_image]
+					session[:image] = session[:mobile_image]
 				else
-					i = session[:desktop_image]
+					session[:image] = session[:desktop_image]
 				end
-				f = open(i)
+				f = open(session[:image])
 				Log.create(type: "Ad", action: "show", data: session[:ad], ip: request.ip, ad_id: session[:ad]["_id"]["$oid"], ad_size: type)
 			rescue
 				f = open("http://s3.amazonaws.com/invtr/ads/avatars/56a1/03e2/dfee/1b00/0300/0000/original/IMAGE_SP.jpg?1453392866")
