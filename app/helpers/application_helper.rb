9f45f70f021fd654b196ab1a56f68066d9956f03
John Davison <johncdavison@gmail.com>
2016-04-21 15:19:25 -0700

    HOTFIX Image url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,30 +5,31 @@ module ApplicationHelper
 	  end
 	end
 	def get_bg_image
-		session[:type] = "desktop"
-		if request.env['HTTP_USER_AGENT'].include? "Android" or request.env['HTTP_USER_AGENT'].include? "iPhone"
-			session[:type] = "mobile"
-		end
-		session[:type] = "mobile"
-		if session[:image] and session[:image_time] > DateTime.now - 1.hour
-			@image = session[:image]
-			Log.create(type: "Ad", action: "show", data: session[:ad], ip: request.ip, ad_id: session[:ad]["_id"], ad_size: session[:type])
-		else
-			@ad = Ad.next
-			session[:ad] = @ad
-			now = DateTime.now
-			@ad.last_served = now
-			@ad.save
-			if session[:type] == "mobile"
-				session[:image] = @ad.mobile_location
-			else
-				session[:image] = @ad.desktop_location
-			end
+	  '/'
+		# session[:type] = "desktop"
+		# if request.env['HTTP_USER_AGENT'].include? "Android" or request.env['HTTP_USER_AGENT'].include? "iPhone"
+		# 	session[:type] = "mobile"
+		# end
+		# session[:type] = "mobile"
+		# if session[:image] and session[:image_time] > DateTime.now - 1.hour
+		# 	@image = session[:image]
+		# 	Log.create(type: "Ad", action: "show", data: session[:ad], ip: request.ip, ad_id: session[:ad]["_id"], ad_size: session[:type])
+		# else
+		# 	@ad = Ad.next
+		# 	session[:ad] = @ad
+		# 	now = DateTime.now
+		# 	@ad.last_served = now
+		# 	@ad.save
+		# 	if session[:type] == "mobile"
+		# 		session[:image] = @ad.mobile_location
+		# 	else
+		# 		session[:image] = @ad.desktop_location
+		# 	end
 
-			session[:image_time] = now
-			Log.create(type: "Ad", action: "show", data: session[:ad], ip: request.ip, ad_id: session[:ad]["_id"], ad_size: session[:type])
+		# 	session[:image_time] = now
+		# 	Log.create(type: "Ad", action: "show", data: session[:ad], ip: request.ip, ad_id: session[:ad]["_id"], ad_size: session[:type])
 
-			@image = session[:image]
-		end
+		# 	@image = session[:image]
+		# end
 	end
 end
